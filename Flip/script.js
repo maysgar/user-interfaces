e44990962799f3ef826ba3554852689008d27991
Gabriel García <100346140@alumnos.uc3m.es>
2017-10-23 11:50:34 +0200

    FINALISIMO v.1

--- Flip/script.js
+++ Flip/script.js
@@ -1,26 +1,60 @@
 
-var images_array = ['./IMAGES/1.jpg','./IMAGES/1.jpg','./IMAGES/2.jpg','./IMAGES/2.jpg','./IMAGES/3.jpg','./IMAGES/3.jpg','./IMAGES/4.jpg','./IMAGES/4.jpg','./IMAGES/5.jpg','./IMAGES/5.jpg','./IMAGES/6.jpg','./IMAGES/6.jpg']; 
+var images_array = ['IMAGES/1.jpg','./IMAGES/1.jpg','./IMAGES/2.jpg','./IMAGES/2.jpg','./IMAGES/3.jpg','./IMAGES/3.jpg','./IMAGES/4.jpg','./IMAGES/4.jpg','./IMAGES/5.jpg','./IMAGES/5.jpg','./IMAGES/6.jpg','./IMAGES/6.jpg']; 
 
 //Array for pushing in only two images to see if they are the same
-var flipped_array = ['0','0'];
+var flipped_array = [];
+var memory_images = [];
 //Number that can only go up to 2 and represents the actual images flipped on board 
 var imagesFlipped = 0;
 
 $(document).ready(function(){
 	$(".front").click(function(ui){
 		$("#"+ui.currentTarget.parentElement.id).css("transform", "rotateY(180deg)");
-		imagesFlipped++;
-		//Pushes into the array the image of the flipped panel 
-		flipped_array.push(ui.currentTarget.parentElement.childNodes[1].firstElementChild.src);
+		checkValidity();
 	});
 });
-$(document).ready(function(){
-	$(".back").click(function(ui){
-		//Passes the id panel to the function 
-		//match(ui.currentTarget.parentElement.id);
-		$("#"+ui.currentTarget.parentElement.id).css("transform", "rotateY(0deg)");
-	});
-});
+
+function checkValidity(id, src){
+	imagesFlipped++;
+	//Pushes into the array the image of the flipped panel, 1st box flipped
+	console.log(imagesFlipped);
+	if(memory_images.length < 2 && flipped_array < 2 && imagesFlipped == 1){
+		memory_images.push(id);
+		flipped_array.push(src);
+		console.log(memory_images);
+		console.log(flipped_array);
+	}
+	//Pushes 2nd box flipped into the array the image of the flipped panel
+	else if(imagesFlipped == 2){
+		memory_images.push(id);
+		flipped_array.push(src);
+		console.log(memory_images);
+		console.log(flipped_array);
+		//Checks both src of the boxes
+		if(flipped_array[0] == flipped_array[1]){
+			//If they are the same, you cannot flip them anymore
+			document.getElementById(memory_images[0]).classList.remove("transform");
+			document.getElementById(memory_images[1]).classList.remove("transform");
+			//Añadir +1 contador
+			//Empty the arrays
+			memory_images = [];
+			flipped_array = [];
+			imagesFlipped = 0;
+		}
+		//Else empty the arrays and rotate back the panels
+		else{
+			rotateAuto();
+			memory_images = [];
+			flipped_array = [];
+			imagesFlipped = 0;	
+		}	
+	}
+}
+
+function rotateAuto(){
+	$("#"+memory_images[0]).css("transform","rotateY(0)");
+	$("#"+memory_images[1]).css("transform","rotateY(0)");
+}
 
 function board(){
 	//Randomizes boxes
@@ -33,7 +67,8 @@ function board(){
 	document.getElementById("main-box").innerHTML = display;
 	for(var i = 0; i<images_array.length; i++){
 		//Set css properties for each different panel
-		$("#panel_"+i).css({"width": "200px","height": "200px" ,"margin": "10px" ,"float": "left" ,"transform-style": "preserve-3d","transition": "all 0.5s"});
+		$("#panel_"+i).css({"width": "200px","height": "200px" ,"margin": "10px" ,"float": "left"});
+		$("#panel_"+i).addClass("transform");
 		//Set cursor when hovering on panels
 		$("#panel_"+i).hover(function(){
 			$(this).css("cursor","pointer");
@@ -50,15 +85,5 @@ function randomBoard(array){
 		array[j] = aux;
 	}
 	return array;
-}
-
-function match(card){
-	if(flipped_array[0] == flipped_array[1] && imagesFlipped == 2){
-		flipped_array = [];
-		//$("#"+card).css("transform","rotateY(0deg)");
-	}	
-	else{
-		//$("#"+card).css("transform","rotateY(0deg)");
-	}
 }
 
